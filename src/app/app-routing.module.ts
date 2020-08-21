74c18b76a9c1dc0422bf6414fbe670199d930c0f
Mochi <mochiko@mochi.com>
2020-08-21 14:29:28 -0400

    routing to sign-up/signin,may change later

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainDisplayComponent } from './main-display/main-display.component'
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
 
 
 const routes: Routes = [
   { path: '', component: MainDisplayComponent, pathMatch: 'full' },
   { path:'signup',component:SignUpComponent,pathMatch:'full'},
+  { path:'signin',component:SignInComponent,pathMatch:'full'},
   { path: '**', redirectTo: '', pathMatch: 'full' },
   
 ];
--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/auth'
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
-
-  constructor() { }
+  email: string;
+  password: string;
+  verifyPassword: string;
+  constructor(private auth : AngularFireAuth) { }
 
   ngOnInit(): void {
   }
-
+  createAccount(){
+    this.auth.createUserWithEmailAndPassword(this.email, this.password)
+      .catch((err) => {
+        console.log(err.code);
+        console.log(err.message);
+      })
+  }
 }
